2404eb44e8947071a65ef3a54a7e106ae5fae909
Alex Solis <git@alexsol.is>
2025-08-25 19:18:56 -0400

    Update styling, primarily Waybar

--- home-manager/shell.nix
+++ home-manager/shell.nix
@@ -23,10 +23,11 @@
       "ctrl+k" = "clear_terminal reset active";
     };
     settings = {
-      window_margin_width = 10;
+      window_margin_width = 5;
       cursor_stop_blinking_after = 15.0;
       confirm_os_window_close = -1;
       clipboard_control = "write-clipboard write-primary read-clipboard read-primary";
+      font_size = 9;
     };
     themeFile = "Catppuccin-Macchiato";
   };
--- home-manager/waybar/scripts/powerdraw.sh
+++ home-manager/waybar/scripts/powerdraw.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ -f /sys/class/power_supply/macsmc-battery/energy_now ]; then
-  powerDraw="󰠰 $(($(cat /sys/class/power_supply/macsmc-battery/power_now) / 1000000))w"
+  powerDraw="$(($(cat /sys/class/power_supply/macsmc-battery/power_now) / 1000000))"
 fi
 
 cat <<EOF
-{ "text":"$powerDraw", "tooltip":"power Draw $powerDraw"}  
+{ "text":"$powerDraw", "tooltip":"Power Draw $powerDraw"}  
 EOF
