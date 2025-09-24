152a856aab68fe1ffae6e335c704246c806c66a4
Alex Solis <git@alexsol.is>
2025-09-24 00:19:37 -0400

    Update todo-rs, but my system doesn't boot

--- home-manager/waybar/scripts/todo.sh
+++ home-manager/waybar/scripts/todo.sh
@@ -2,7 +2,7 @@
 
 todoText="$(
   cd ~
-  todo list -d | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's#"#\\"#g'
+  todo list -d -nc | aha -n | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's#"#\\"#g'
   # That sed converts newlines to \n. I have no idea how
 )"
 
