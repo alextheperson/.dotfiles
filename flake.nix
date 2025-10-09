ec4d22c6dc6726c6e95726a5160b32c412b83641
Alex Solis <git@alexsol.is>
2025-10-08 21:43:05 -0400

    Locked everything, slowly unlock to identify boot-issue

--- flake.nix
+++ flake.nix
@@ -8,23 +8,23 @@
     };
 
     dmm = {
-      url = "tarball+https://git.fawkes.io/mtnash/dmm/archive/stable.tar.gz";
+      url = "tarball+https://git.fawkes.io/mtnash/dmm/archive/stable.tar.gz/?rev=e17059b308699b7fe60764b5c22e0872af04939b";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     todo-rs = {
-      url = "github:alextheperson/todo-rs";
+      url = "github:alextheperson/todo-rs/?rev=cbb1ab4469397b7281dcbadf16c54290f43ee1b2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/?rev=9eab59f3e71ea3a725e4817d8dcf0da0824ad19d";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    catppuccin.url = "github:catppuccin/nix";
-    nix-alien.url = "github:thiagokokada/nix-alien";
-    hyprland.url = "github:hyprwm/Hyprland";
+    catppuccin.url = "github:catppuccin/nix/?rev=d75e3fe67f49728cb5035bc791f4b9065ff3a2c9";
+    nix-alien.url = "github:thiagokokada/nix-alien/?rev=e62b3ad75e8c9a5e505fc78b9b40eed1178634cb";
+    hyprland.url = "github:hyprwm/Hyprland/?rev=88326075743a677e76645ff163b392490419d4de";
   };
   outputs = inputs@{ self, nixpkgs, home-manager, apple-silicon, catppuccin, nix-alien, dmm, todo-rs, ... }: {
     nixosConfigurations.serafina = nixpkgs.lib.nixosSystem {
